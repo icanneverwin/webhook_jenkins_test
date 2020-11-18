6e5bf9fe3e20af95e6647bc746367690c57c6208
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-11-18 13:17:14 +0300

    Update move_rpd.sh

--- move_rpd.sh
+++ move_rpd.sh
@@ -4,7 +4,7 @@
 echo "Preparing for RPD file movement"
 echo "Jenkins Workspace is ${WORKSPACE}"
 
-RPD=/var/lib/jenkins/workspace/test_jenkins_pipeline/liverpd.rpd
+RPD=${WORKSPACE}/liverpd.rpd
 TEMP_DIR=/tmp/jenkins_tmp_data
 
 # check if RPD exists
