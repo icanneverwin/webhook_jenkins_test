9c0ff04dd4f97d0e2cff2d7906e149e160fcb4c9
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-11-20 19:26:46 +0300

    Update showGitDiff.groovy

--- jenkins/showGitDiff.groovy
+++ jenkins/showGitDiff.groovy
@@ -1,6 +1,6 @@
 def showGitDiff(String HEAD_SHA, String BASE_SHA) {
   /*execute git diff command to retrieve diff between input commits */
-  def DIFFLOG = sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA}", returnStdout: true)
-  return DIFFLOG
+  /*def DIFFLOG = sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA}", returnStdout: true) */
+  sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA} > difflog.txt", returnStdout: true)
 }
 return this
