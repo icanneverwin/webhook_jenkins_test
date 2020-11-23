30bbaf32bfab6860e6c77e36df84b65062f2f9be
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-11-23 17:45:51 +0300

    Update showGitDiff.groovy

--- jenkins/showGitDiff.groovy
+++ jenkins/showGitDiff.groovy
@@ -5,12 +5,12 @@ def showGitDiff(String HEAD_SHA, String BASE_SHA) {
   /*def DIFFLOG = sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA}", returnStdout: true) */
 
   // create difflog.txt file
-  sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA} > ${deploylist}", returnStdout: true)
+  sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA} > difflog.txt", returnStdout: true)
 }
 
 def rpdDiff() {
   def RPD_BUILD = false
-  if (sh(script: "grep -e liverpd ${deploylist}", returnStatus: true) == "0") {
+  if (sh(script: "grep -e liverpd difflog.txt", returnStatus: true) == "0") {
     RPD_BUILD = true
   }
   return RPD_BUILD
@@ -18,7 +18,7 @@ def rpdDiff() {
 
 def webDiff() {
   def WEB_BUILD = false
-  if (sh(script: "grep -e WEB/ ${deploylist}", returnStatus: true) == "0") {
+  if (sh(script: "grep -e WEB/ difflog.txt", returnStatus: true) == "0") {
     WEB_BUILD = true
   }
   return WEB_BUILD
@@ -26,7 +26,7 @@ def webDiff() {
 
 def dbDiff() {
   def DB_BUILD = false
-  if (sh(script: "grep -e DB/ ${deploylist}", returnStatus: true) == "0") {
+  if (sh(script: "grep -e DB/ difflog.txt", returnStatus: true) == "0") {
     DB_BUILD = true
   }
   return DB_BUILD
@@ -34,7 +34,7 @@ def dbDiff() {
 
 def shDiff() {
   def SHELL_BUILD = false
-  if (sh(script: "grep -e SHELL/ ${deploylist}", returnStatus: true) == "0") {
+  if (sh(script: "grep -e SHELL/ difflog.txt", returnStatus: true) == "0") {
     SHELL_BUILD = true
   }
   return SHELL_BUILD
