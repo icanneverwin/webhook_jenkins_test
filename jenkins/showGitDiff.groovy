ca6aa9ae2034d0041217a52532b58c01923a112e
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-11-23 17:36:58 +0300

    Update showGitDiff.groovy

--- jenkins/showGitDiff.groovy
+++ jenkins/showGitDiff.groovy
@@ -1,27 +1,42 @@
+def deploylist = "difflog.txt"
+
 def showGitDiff(String HEAD_SHA, String BASE_SHA) {
   /*execute git diff command to retrieve diff between input commits */
   /*def DIFFLOG = sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA}", returnStdout: true) */
-  def RPD_BUILD = false
-  def WEB_BUILD = false
-  def DB_BUILD = false
-  def SHELL_BUILD = false
-  def deploylist = "difflog.txt"
 
   // create difflog.txt file
   sh(script: "git diff --name-only --diff-filter=ACMR ${HEAD_SHA}...${BASE_SHA} > ${deploylist}", returnStdout: true)
+}
 
-  // identify deployment type based on extracted diff
-  if (sh(script: "grep -e liverpd ${deploylist}", returnStatus: true) == "0") {
+def rpdDiff() {
+  def RPD_BUILD = false
+  if (sh(script: "grep -e liverpd ${deploylist}", returnStatus: true) == "0")) {
     RPD_BUILD = true
   }
-  else if (sh(script: "grep -e WEB/ ${deploylist}", returnStatus: true) == "0") {
+  return RPD_BUILD
+}
+
+def webDiff() {
+  def WEB_BUILD = false
+  if (sh(script: "grep -e WEB/ ${deploylist}", returnStatus: true) == "0") {
     WEB_BUILD = true
   }
-  else if (sh(script: "grep -e DB/ ${deploylist}", returnStatus: true) == "0") {
+  return WEB_BUILD
+}
+
+def dbDeployDiff() {
+  def DB_BUILD = false
+  if (sh(script: "grep -e DB/ ${deploylist}", returnStatus: true) == "0") {
     DB_BUILD = true
   }
-  else if (sh(script: "grep -e SHELL/ ${deploylist}", returnStatus: true) == "0") {
+  return DB_BUILD
+}
+
+def dbDeployDiff() {
+  def SHELL_BUILD = false
+  if (sh(script: "grep -e SHELL/ ${deploylist}", returnStatus: true) == "0") {
     SHELL_BUILD = true
   }
+  return SHELL_BUILD
 }
 return this
