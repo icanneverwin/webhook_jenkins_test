5b70589d89e41c1b2607f4b3ec33d4de28cd1ca0
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-11-26 15:37:30 +0300

    Update move_rpd.sh

--- cmrm/bin/move_rpd.sh
+++ cmrm/bin/move_rpd.sh
@@ -4,16 +4,39 @@
 echo "Preparing for RPD file movement"
 echo "Jenkins Workspace is ${WORKSPACE}"
 
-RPD=${WORKSPACE}/liverpd.rpd
-TEMP_DIR=/tmp/jenkins_tmp_data/
+XML_PATCH=${WORKSPACE}/Application/Patches/rpd_change.xml
+DEST_DIR=${WORKSPACE}/Application/RPD
+RPD_FILE=${WORKSPACE}/tmp/liverpd_`date +"%Y%m%d_%H%M"`
 
 # check if RPD exists
-if [[ ! -e $RPD ]] ; then
+if [[ ! -e $XML_PATCH ]] ; then
   echo "File does not exist, job failed..."
   exit 1
-else 
-  echo "RPD file exists, moving..."
-  mv $RPD $TEMP_DIR
+else
+  # prepare tmp dir
+  mkdir ${WORKSPACE}/tmp
+  RC=$?
+  if [[ $RC -ne 0 ]] ; then
+    echo "Oops, something bad happened! Cannot create ${WORKSPACE}/tmp folder"
+    exit 1
+  fi
+
+  echo "XML Patch found, compiling RPD file...."
+  touch "${RPD_FILE}"
+  echo "file created `date`" >> "${RPD_FILE}"
+
+  # for testing purposes
+  cp "${RPD_FILE}" /tmp/jenkins_tmp_data/
+  RC=$?
+  if [[ $RC -eq 0 ]] ; then
+    echo "RPD has been moved successfully"
+  else
+    echo "Oops, something bad happened! Cannot copy the file ${RPD}"
+	exit 1
+  fi
+
+  echo "moving file"
+  mv "${RPD_FILE}" "${DEST_DIR}"
   RC=$?
   if [[ $RC -eq 0 ]] ; then
     echo "RPD has been moved successfully"
