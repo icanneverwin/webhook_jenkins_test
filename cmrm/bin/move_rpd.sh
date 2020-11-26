9cadb21a1caa652b96161dcccbc1a9ff722d9c6b
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-11-26 20:18:18 +0300

    Update move_rpd.sh

--- cmrm/bin/move_rpd.sh
+++ cmrm/bin/move_rpd.sh
@@ -4,46 +4,52 @@
 echo "Preparing for RPD file movement"
 echo "Jenkins Workspace is ${WORKSPACE}"
 
-XML_PATCH=${WORKSPACE}/Application/Patches/rpd_change.xml
-DEST_DIR=${WORKSPACE}/Application/RPD
-RPD_FILE=${WORKSPACE}/tmp/liverpd_`date +"%Y%m%d_%H%M"`
+DIFFLOG_LOCATION="${WORKSPACE}/difflog.txt"
+RPD_PREFIX="Applicacation/Patches"
+DEST_DIR="${WORKSPACE}/Application/RPD"
+RPD_FILE="${WORKSPACE}/tmp/liverpd_$(date +"%Y%m%d_%H%M")"
 
-# check if RPD exists
-if [[ ! -e $XML_PATCH ]] ; then
-  echo "File does not exist, job failed..."
+DIFFLOG=$(grep -E "^${RPD_PREFIX}" "${DIFFLOG_LOCATION}")
+
+if [[ -z ${DIFFLOG} ]] ; then
+  echo "INFO : Difflog is empty or no files fetched from a repository."
+  echo "INFO : Nothing to build. Exiting"
+  exit 0
+fi
+
+# prepare tmp dir
+mkdir "${WORKSPACE}"/tmp
+RC=$?
+if [[ $RC -ne 0 ]] ; then
+  echo "Oops, something bad happened! Cannot create ${WORKSPACE}/tmp folder"
   exit 1
+fi
+
+for i in ${DIFFLOG} ; do
+  echo "XML Patch found, processing ${i} ..."
+  echo "patch ${i} compiled" >> "${RPD_FILE}"
+done
+
+
+# for testing purposes
+echo "copying ${RPD_FILE}"
+cp "${RPD_FILE}" /tmp/jenkins_tmp_data/
+RC=$?
+if [[ $RC -eq 0 ]] ; then
+  echo "RPD has been moved successfully"
 else
-  # prepare tmp dir
-  mkdir ${WORKSPACE}/tmp
-  RC=$?
-  if [[ $RC -ne 0 ]] ; then
-    echo "Oops, something bad happened! Cannot create ${WORKSPACE}/tmp folder"
-    exit 1
-  fi
+  echo "Oops, something bad happened! Cannot copy the file ${RPD}"
+exit 1
+fi
 
-  echo "XML Patch found, compiling RPD file...."
-  touch "${RPD_FILE}"
-  echo "file created `date`" >> "${RPD_FILE}"
-
-  # for testing purposes
-  cp "${RPD_FILE}" /tmp/jenkins_tmp_data/
-  RC=$?
-  if [[ $RC -eq 0 ]] ; then
-    echo "RPD has been moved successfully"
-  else
-    echo "Oops, something bad happened! Cannot copy the file ${RPD}"
-	exit 1
-  fi
-
-  echo "moving file"
-  mv "${RPD_FILE}" "${DEST_DIR}"
-  RC=$?
-  if [[ $RC -eq 0 ]] ; then
-    echo "RPD has been moved successfully"
-  else
-    echo "Oops, something bad happened! Cannot move the file ${RPD}"
-	exit 1
-  fi
+echo "moving ${RPD_FILE}"
+mv "${RPD_FILE}" "${DEST_DIR}"
+RC=$?
+if [[ $RC -eq 0 ]] ; then
+  echo "RPD has been moved successfully"
+else
+  echo "Oops, something bad happened! Cannot move the file ${RPD}"
+exit 1
 fi
 
 echo "all checks are done, exiting..."
