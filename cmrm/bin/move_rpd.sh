546fa8ea71c3e9b9320af36970e7328556667c0c
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-12-01 11:59:19 +0300

    Update move_rpd.sh

--- cmrm/bin/move_rpd.sh
+++ cmrm/bin/move_rpd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+. ${WORKSPACE}/cmrm/bin/supp_func.sh
+
 ###
 echo "Preparing for RPD file movement"
 echo "Jenkins Workspace is ${WORKSPACE}"
@@ -8,6 +10,8 @@ DIFFLOG_LOCATION="${WORKSPACE}/difflog.txt"
 RPD_PREFIX="Application/Patches"
 DEST_DIR="${WORKSPACE}/Application/RPD"
 RPD_FILE="${WORKSPACE}/tmp/liverpd_$(date +"%Y%m%d_%H%M")"
+
+GIT_BRANCH="rpd_prod"
 
 DIFFLOG=$(grep -E "^${RPD_PREFIX}" "${DIFFLOG_LOCATION}")
 
@@ -36,7 +40,7 @@ echo "copying ${RPD_FILE}"
 cp "${RPD_FILE}" /tmp/jenkins_tmp_data/
 RC=$?
 if [[ $RC -eq 0 ]] ; then
-  echo "RPD has been moved successfully"
+  echo "RPD has been copied to /tmp/jenkins_tmp_data/"
 else
   echo "Oops, something bad happened! Cannot copy the file ${RPD}"
 exit 1
@@ -46,10 +50,20 @@ echo "moving ${RPD_FILE}"
 mv "${RPD_FILE}" "${DEST_DIR}"
 RC=$?
 if [[ $RC -eq 0 ]] ; then
-  echo "RPD has been moved successfully"
+  echo "RPD has been moved to ${DEST_DIR}"
 else
   echo "Oops, something bad happened! Cannot move the file ${RPD}"
 exit 1
+fi
+
+
+git_push "${DEST_DIR}"/"${RPD_FILE}" "${GIT_BRANCH}" "${params.PR_TITLE}"
+RC=$?
+if [[ $RC -eq 0 ]] ; then 
+  echo "pushed to repo successfully"
+else
+  echo "error during pushing to repo, exiting..."
+  exit 1
 fi
 
 echo "all checks are done, exiting..."
