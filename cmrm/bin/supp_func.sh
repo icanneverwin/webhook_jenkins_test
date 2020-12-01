b86f7b5bedb25928c09b7201f9a9623b30b9502e
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-12-01 11:59:51 +0300

    Create supp_func.sh

---
+++ cmrm/bin/supp_func.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+git_push() {
+  # push file to the branch
+  FILENAME=$1
+  BRANCH=$2
+  COMMIT_TITLE=$3
+
+  #DEST_BRANCH="rpd_prod"
+
+  git config user.email "jogn.doe@example.com"
+  git config user.name "John Doe"
+
+  GIT_STATUS=$(git status --porcelain)
+
+  if [[ -n "${GIT_STATUS}" ]] ; then
+    # checking if file is in the list
+    grep -e "${FILENAME}" ${GIT_STATUS}
+    RC=$?
+    if [[ $RC -eq 0 || $RC -eq 2 ]] ; then
+      echo "file not found in git status output, exiting..."
+      exit 1
+    else
+      git add "${FILENAME}"
+      git commit -m "${COMMIT_TITLE}"
+      git push origin HEAD:${BRANCH}
+    fi
+  else 
+    echo "no changes, exiting"
+    exit 1
+  fi
+}
