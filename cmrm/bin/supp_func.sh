d99e1167d4e0cf13c5b690238f9922970f4d2a58
icanneverwin <51402162+icanneverwin@users.noreply.github.com>
2020-12-03 14:22:06 +0300

    Update supp_func.sh

--- cmrm/bin/supp_func.sh
+++ cmrm/bin/supp_func.sh
@@ -11,27 +11,21 @@ git_push() {
   echo "BRANCH is ${BRANCH}"
   echo "COMMIT_TITLE is ${COMMIT_TITLE}"
 
-  #DEST_BRANCH="rpd_prod"
-
   git config user.email "jogn.doe@example.com"
   git config user.name "John Doe"
 
-  GIT_STATUS=$(git status --porcelain)
+  echo $(git status --porcelain) | grep -o "${FILENAME}"
+  RC=$?
 
-  if [[ -n "${GIT_STATUS}" ]] ; then
-    # checking if file is in the list
-    grep -o "${FILENAME}" "${GIT_STATUS}"
-    RC=$?
-    if [[ $RC -eq 0 || $RC -eq 2 ]] ; then
-      echo "file not found in git status output, exiting..."
-      exit 1
-    else
-      git add "${FILENAME}"
-      git commit -m "${COMMIT_TITLE}"
-      git push origin HEAD:"${BRANCH}"
-    fi
-  else 
-    echo "no changes, exiting"
+  if [[ $RC -eq 0 || $RC -eq 2 ]] ; then
+    echo "file not found in git staging area, exiting..."
     exit 1
-  fi
+  else
+    echo "git add ${FILENAME}"
+    git add "${FILENAME}"
+    echo "git commit -m ${COMMIT_TITLE}"
+    git commit -m "${COMMIT_TITLE}"
+    echo "git push origin HEAD:${BRANCH}"
+    git push origin HEAD:"${BRANCH}"
+  fi 
 }
